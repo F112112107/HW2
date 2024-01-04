2c6280763926380cac1a19905fa052b1b64c3afd
f112112107 <f112112107@nkust.edu.tw>
2024-01-04 13:56:16 +0800

    Initial commit

---
+++ project_2.sdk/HW_2/Debug/src/helloworld.d
@@ -0,0 +1,43 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ project_2.sdk/HW_2/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../HW_2_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../HW_2_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
